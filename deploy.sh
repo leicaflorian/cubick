cc4ae70ba89d8d6654046a46232209153c422712
leicaflorian <florian.leica@gmail.com>
2022-05-27 23:16:20 +0200

    deploy

--- deploy.sh
+++ deploy.sh
@@ -2,9 +2,9 @@
 # abort on errors
 set -e
 # build
-yarn run build
+# yarn run build
 # navigate into the build output directory
-cd dist
+# cd dist
 # if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
 git init
